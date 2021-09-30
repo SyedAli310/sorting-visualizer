7dd5907c108512b1d7b301bd39d08b3e16574e92
SyedAli310 <alizaidi310@gmail.com>
2021-09-30 18:47:44 +0530

    Ui rewamp (added bootstrap)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 const array = document.querySelector('.array')
-const delay = 300
+const delay = 200
 
 function showArr(arr){
     array.innerHTML=''
@@ -9,23 +9,19 @@ function showArr(arr){
         elt.classList.add('array-el')
         $(elt).animate({height:el*2},200)
         $(elt).width('25px')
-        $(elt).text(el)
-        $(elt).css('background','lightgreen')
+        $(elt).html(`<span class='elt-label text-monospace'>${el}</span>`)
+        $(elt).css('background','#337ab7')
         array.appendChild(elt)
     })
     
 }
 
 $('#randomise').on('click',()=>{
-    arr = [ Math.ceil(Math.random()*100 + 5),
-            Math.ceil(Math.random()*100 + 5),
-            Math.ceil(Math.random()*100 + 5),
-            Math.ceil(Math.random()*100 + 5),
-            Math.ceil(Math.random()*100 + 5),
-            Math.ceil(Math.random()*100 + 5),
-            Math.ceil(Math.random()*100 + 5),
-            Math.ceil(Math.random()*100 + 5),
-    ]
+    arr = []
+    for(let i=0 ; i<20 ; i++){
+        arr.push(Math.ceil(Math.random()*100 + 5))
+    }
+    console.log('Array length: ',arr.length);
     showArr(arr)
 })
 
@@ -34,9 +30,16 @@ function swap(el1,el2){
     el1.style.height = el2.style.height
     el2.style.height = temp1
 
-    let temp2 = el1.innerText
-    el1.innerText = el2.innerText
-    el2.innerText = temp2
+    // console.log(el1.firstChild.innerText);
+    // let temp2 = el1.innerText
+    // el1.innerText = el2.innerText
+    // el2.innerText = temp2
+}
+
+function swap2(lb1,lb2){
+    let temp2 = lb1.innerText
+    lb1.innerText = lb2.innerText
+    lb2.innerText = temp2
 }
 
 function wait(ms){
@@ -47,21 +50,27 @@ function wait(ms){
 
 async function bubbleSort(){
     const array = document.querySelectorAll('.array-el')
+    const labels = document.querySelectorAll('.elt-label')
 
     for(let i=0;i<array.length-1;i++){
         for(let j=0;j<array.length-i-1;j++){
-            array[j].style.background ='yellow'
-            array[j+1].style.background ='yellow'
+            array[j].style.background ='#5bc0de'
+            array[j+1].style.background ='#5bc0de'
             if(parseInt(array[j].style.height) > parseInt(array[j+1].style.height)){
                 await wait(delay)
                 swap(array[j],array[j+1])
+                swap2(labels[j],labels[j+1])
             }
-            array[j].style.background = 'lightgreen'
-            array[j+1].style.background = 'lightgreen'
+            array[j].style.background = '#337ab7'
+            array[j+1].style.background = '#337ab7'
         }
-        array[array.length-1-i].style.background = 'pink'
+        array[array.length-1-i].innerHTML = `<span class='elt-label text-monospace text-success'>${labels[labels.length-1-i].innerText}</span>`
+        array[array.length-1-i].style.background = '#5cb85c'
+        array[array.length-1-i].style.color = 'white'
     }
-    array[0].style.background = 'pink'
+    array[0].innerHTML = `<span class='elt-label text-monospace text-success'>${labels[0].innerText}</span>`
+    array[0].style.background = '#5cb85c'
+    array[0].style.color = 'white'
 }
 
 $('#bubble-sort').on('click', async (e)=>{
